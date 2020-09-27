e8778196d9c9e3908ff49019be54615cf46fc0fd
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-09-27 19:56:23 +0200

    Initial commit

---
+++ src/main/java/demo/morodev/moroWebApp/MoroWebAppApplication.java
@@ -0,0 +1,13 @@
+package demo.morodev.moroWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoroWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoroWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/morodev/moroWebApp/MoroWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.morodev.moroWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
